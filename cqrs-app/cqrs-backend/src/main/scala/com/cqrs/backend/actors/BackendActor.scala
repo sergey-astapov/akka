96e9ffc619d29348a5221da977ee620718aeaef0
sergey.astapov <serg.astapov@gmail.com>
2015-02-22 01:20:41 +0300

    - add kamon

--- cqrs-app/cqrs-backend/src/main/scala/com/cqrs/backend/actors/BackendActor.scala
+++ cqrs-app/cqrs-backend/src/main/scala/com/cqrs/backend/actors/BackendActor.scala
@@ -3,13 +3,19 @@ package com.cqrs.backend.actors
 import akka.actor._
 import akka.event.slf4j.SLF4JLogging
 import com.cqrs.backend.model.Protocol.{StateResult, GetState}
+import kamon.trace.TraceRecorder
 
 class BackendActor extends Actor with SLF4JLogging {
   var total: Int= 0
 
   def receive = {
     case m: GetState =>
-      total += 1
-      sender() ! StateResult(total)
+      implicit val system = context.system
+      TraceRecorder.withNewTraceContext("sample-trace") {
+        total += 1
+        log.debug(s"Total=$total")
+        sender() ! StateResult(total)
+      }
+      TraceRecorder.finish()
   }
 }
--- cqrs-app/cqrs-backend/src/main/scala/com/cqrs/backend/node/BackendNode.scala
+++ cqrs-app/cqrs-backend/src/main/scala/com/cqrs/backend/node/BackendNode.scala
@@ -5,7 +5,17 @@ import com.cqrs.backend.actors.BackendActor
 import com.typesafe.config.ConfigFactory
 
 object BackendNode extends App {
+  checkAspectj
+
   val config = ConfigFactory.load().getConfig("BackendSys")
   implicit val system = ActorSystem("BackendSys", config)
   val remote = system.actorOf(Props[BackendActor], "BackendActor")
+
+  def checkAspectj():Unit = {
+    try {
+      org.aspectj.weaver.loadtime.Agent.getInstrumentation
+    } catch {
+      case e: Throwable => println(s"No AspectJ error: $e")
+    }
+  }
 }
